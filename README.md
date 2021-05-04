1b9a4ee9ec4c0f9861572cf31f788121f8771ec3
Peter Eldritch <peter.jirak@gmail.com>
2021-05-04 02:37:31 -0700

    First commit of my solution. I have initialized npm so I am using npm to manage my dependencies.
    So far in my solution I:
    
    1. Load the contents of the output file if there are any.
    2. Read the contents of thew CSV file one line at a time.
    3. Skip any entries in the CSV file that already have results
       in the output file based on store number.
    
    The problem requires that I call an HTTP GET API endpoint to get
    geolocation information for the given store number based on the store's
    address.
    
    I have started to implement that. I have a Node.JS function named
    geolocateStoreAddress which uses the axios package module to make an
    HTTP GET request to the endpoint with the required parameters.
    
    The axios call has a .then which is called if the HTTP GET request
    succeeds and a .catch that gets called if the HTTP GET request fails.
    
    I have started a getHttpResponseHandler which is a JavaScript function
    that returns a callback (JavaScript function for processing the
    HTTP response from the HTTP GET request). I use that approach because
    I need the storeNumber and the address as a part of handling the
    response though there may be an easier/more straight forward way to handle
    this issue.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # lululemon-geolocate
 Interview problem from Lululemon - take in an input CSV file, call an API endpoint to get geolocation information, generate an output JSON file that contains the results.
+
+# NodeJS Test Project
+
+Write a script that geolocates some of our lululemon store locations!
+
+This script should iterate through the `input.csv` csv file, and query the [PositionStack API](https://positionstack.com/documentation#forward_geocoding) for the required data.
+
+You should populate output.json with the required data from the responses.
+The first entry is completed for you as an example:
+
+ ```json
+{
+  "00120": {
+    "street_address": "970 Robson Street",
+    "locality": "Vancouver",
+    "region": "British Columbia",
+    "country": "Canada",
+    "geopoint": {
+      "lat": 49.283026,
+      "long": -123.122826
+    }
+  }
+}
+```
+
+## Test Guidelines
+
+- You are encouraged to use the entire internet during this test. Searching for libraries and documentation is part of software development.
+- Learning specific package managers or libraries is not part of the test, use whatever you like.
+- Token creation is not part of the test, if you have trouble with your token please ask for help.
+- Feel free to ask for clarification during the test.
+
+Completion Milestones:
+
+Feel free to do parts in any order.
+
+1. CSV Parsed
+    - You should not edit `input.csv`, and your program should read this file during it's operation.
+2. Geolocation Queried
+    - Your program successfully locates a store.
+3. JSON Written
+    - Your program outputs the desired json file `output.json`. Feel free to write over or append data to this file, but do not change it's schema.
+
+
+## Documentation
+
+[PositionStack API](https://positionstack.com/documentation#forward_geocoding)
+
+## Notes
+
+It seems the positionstack free API does not support `https` so you will need to use the `http` endpoint.
+
+If there are multiple responses from the geolocation API, use only the first one.
---
+++ geolocate.js
@@ -0,0 +1,175 @@
+'use strict';
+
+const inputCsv = './input.csv';
+const outputJson = './output.json';
+
+const apiKey = 'd54234f9b3b1ddf124c70cf33484e139';
+const baseUrl = 'https://api.positionstack.com';
+
+let outputJsonObj = null;
+let lineCount = 0;
+
+// Complete me!
+// Notes:
+// 1. How to handle it if output.json already exists:
+//
+//    I have decided in my implementation that if output.json already exists,
+//    then the program will load the contents of output.json. As it goes through
+//    the lines in input.csv if it encounters a store number for which it has
+//    already performed the geolocate operation according to the data in the
+//    existing output.json, then it skips the operation for that given store
+//    number. It assumes the results in output.json are correct.
+//
+//    So this implementation is stateful.
+//
+// 2. Use of axios:
+//
+//    axios is a promise based HTTP client for both browser-side JavaScript and
+//    server-side Node.JS.
+//
+//    https://www.npmjs.com/package/axios
+
+const fs = require('fs');
+const axios = require('axios');
+
+function loadOutputJson(filename) {
+    let outputJsonText = null;
+    try {
+        outputJsonText = fs.readFileSync(filename, 'utf8');
+    } catch(err) {
+        console.error(
+            `Failed to read in the file: ${filename}: ${err}`
+        );
+        throw(err);
+    }
+
+    if (!outputJsonText) {
+        console.error(
+            `No data loaded from ${filename}`
+        );
+        throw(new TypeError(`No data loaded from ${filename}`));
+    }
+
+    let jsonObj = null;
+    try {
+        jsonObj = JSON.parse(outputJsonText);
+    } catch (err) {
+        if (err instanceof SyntaxError) {
+            const failureMsg = `Failed to load the data from the file ` +
+                               `${filename}. That file does not contain a ` +
+                               `valid JSON object: ${err}`
+            console.error(
+                failureMsg
+            );
+            throw(failureMsg);
+        } else {
+            const failureMsg = `Failure encountered while attempting to ` +
+                               `JSON parse data from the file ` +
+                               `${filename}: ${err}`;
+                               console.error(
+                                failureMsg
+                            );
+                            throw(failureMsg);
+                        }
+    }
+
+    if (!jsonObj) {
+        throw( new InternalError(`Failed to load valid JSON data ` +
+                                 `from ${filename}.`));
+    }
+
+    return jsonObj;
+}
+
+function getOutputJsonObject(filename) {
+    try {
+        if (fs.existsSync(filename)) {
+            return loadOutputJson(filename);
+        } else {
+            return {};
+        }
+    } catch (err) {
+        console.error(err);
+        throw(err);
+    }
+}
+
+function handleHttpResponseFromApiCall(reponse) {
+
+}
+
+function getHttpResponseHandler(storeNumber, address) {
+    const handler = (response) => {
+
+    }
+
+    return handler;
+}
+
+function geolocateStoreAddress(storeNumber, address) {
+    axios({
+        method: 'get',
+        baseUrl: baseUrl,
+        url: '/v1/forward',
+        params: {
+            access_key,
+            query: address
+        }
+    }).then(
+
+    ).catch(
+
+    );
+}
+
+function processLine(line) {
+    lineCount += 1;
+    line = line.trim();
+    if (lineCount === 1) {
+        if (line.toLowerCase() !== 'store,address') {
+            throw(new SyntaxError(`The first line of the input CSV file ` +
+                                  `${inputCsv} is not the header 'Store,Address'.`));
+        }
+    } else {
+        const matches = line.match(/^([0-9]+),(.+)$/);
+        if (!matches) {
+            throw(new SyntaxError(`Error encountered on line number ${lineCount} ` +
+                                  `in file ${inputCsv}. Expected a store number, ` +
+                                  `followed by a comma, followed by an address. ` +
+                                  `Line was not as expected.`));
+        }
+
+        const storeNumber = matches[1];
+        const inputAddress = matches[2].trim();
+
+        if (outputJsonObj[storeNumber]) {
+            console.log(
+                `Skipping line number ${lineCount} in the file ${inputCsv}. ` +
+                `That line is for store number ${storeNumber}, but we ` +
+                `previously already performed the geolocate operation for that store.`
+            );
+        } else {
+
+        }
+    }
+}
+
+function main() {
+    if (!fs.existsSync(inputCsv)) {
+        const failureMsg = `The input CSV file ${inputCsv} does not exist.`;
+        console.error(failureMsg);
+        throw(new TypeError(failureMsg));
+    }
+    const inputFileStats = fs.statSync(inputCsv);
+    if (inputFileStats.isDirectory()) {
+        const failureMsg = `${inputCsv} was specified as the input CSV file. ` +
+                           `However, that path is a directory and not a file.`;
+        console.error(failureMsg);
+        throw(new TypeError(failureMsg));
+    }
+    outputJsonObj = getOutputJsonObject(outputJson);
+    const lineReader = require('line-reader');
+    lineReader.eachLine(inputCsv, processLine);
+}
+
+main();
